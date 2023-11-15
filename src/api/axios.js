553024c9e84f06e1c756acdc6b0b112c027ba45d
Jason Yang <didwotjs120@gmail.com>
2023-11-15 11:06:44 +0900

    .

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
 	baseURL: 'https://api.themoviedb.org/3', //계속 반복되는 부분을 인스턴스화 한다
 	params: {
-		api_key: '048e5d051402860f82fdcef2a92282fb',
+		api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
 		language: 'ko-KR',
 	},
 });
